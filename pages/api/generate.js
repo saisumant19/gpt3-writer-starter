8fd849e352447be8366c07dab9bb8f71a9dba0e7
Sai Sumant Kancharla <sumantk1904@gmail.com>
2023-12-26 00:19:11 -0700

    Update generate.js
    added openAI api

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -14,7 +14,7 @@ const generateAction = async (req, res) => {
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
   const baseCompletion = await openai.createCompletion({
-    model: 'gpt-4-0613',
+    model: 'gpt-3.5-turbo-1106',
     prompt: `${basePromptPrefix}${req.body.userInput}\n`,
     temperature: 0.7,
     max_tokens: 250,
